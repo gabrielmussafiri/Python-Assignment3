cb3e2718588056e500a3f4ea88397002f4eb9e63
gabrielmussafiri <musgabriel2@gmail.com>
2025-04-21 13:08:18 +0200

    assigment

---
+++ main.py
@@ -0,0 +1,15 @@
+def large_power(base , exponent):
+    result = base ** exponent
+    if result > 5000:
+        return True
+    else:
+        return False
+
+def divisible_by_ten(num):
+    if num % 10 == 0:
+        return True
+    else:
+        return False
+
+
+    
